96bc0a50983552a820dc46ff4f7459fc292dcc72
Rabi Giri <np01cp4a210220@islingtoncollege.edu.np>
2024-04-29 09:00:43 +0545

    Sign in and Sign up page completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { AppLayout } from './components';
-import { PageNotFound } from './pages';
-import HomePage from './pages/HomePage';
-import { useUserAuthContext } from './utils';
+import { AppLayout } from "./components";
+import { PageNotFound } from "./pages";
+import HomePage from "./pages/HomePage";
+import { useUserAuthContext } from "./utils";
+import ForgotPassword from "./pages/Authentication/ForgotPassword";
+import ChangePassword from "./pages/Authentication/ChangePassword";
+import AuthenticationPage from "./pages/Authentication/AuthenticationPage";
 
 function App() {
   const { currentUser } = useUserAuthContext();
 
   return (
     <>
-      <ToastContainer position='bottom-right' />
+      <ToastContainer position="bottom-right" />
 
       <Routes>
-        <Route path='/' element={<AppLayout />}>
+        <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
         </Route>
-        <Route path='/*' element={<PageNotFound />} />
+        <Route path="/authentication" element={<AuthenticationPage />}></Route>
+
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
       {/* </Suspense> */}
     </>
@@ -28,4 +36,3 @@ function App() {
 }
 
 export default App;
-
--- src/routes/PostRoutes.jsx
+++ src/routes/PostRoutes.jsx
@@ -3,23 +3,23 @@ import { PostDetails, PostForm, Posts } from "../pages";
 import { AuthorizeRoute } from "../components";
 
 const PostRoutes = () => {
-    return (
-        <Routes>
-            <Route index element={<Posts />} />
-            <Route path=":postID" element={<PostDetails />} />
-            <Route
-                path="new"
-                element={
-                    <AuthorizeRoute
-                        pathToNavigateIfNotAuthorized="/posts"
-                        notAllowedRoles={["user"]}
-                    >
-                        <PostForm />
-                    </AuthorizeRoute>
-                }
-            />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route index element={<Posts />} />
+      <Route path=":postID" element={<PostDetails />} />
+      <Route
+        path="new"
+        element={
+          <AuthorizeRoute
+            pathToNavigateIfNotAuthorized="/posts"
+            notAllowedRoles={["user"]}
+          >
+            <PostForm />
+          </AuthorizeRoute>
+        }
+      />
+    </Routes>
+  );
 };
 
 export default PostRoutes;
