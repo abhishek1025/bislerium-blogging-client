a4fc7adb9278706b5739c2170ff48fc0efec60a0
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2024-05-03 08:11:19 +0545

    Ui fixes

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,32 +1,37 @@
-import { Editor } from "@tinymce/tinymce-react";
-import { PropTypes } from "prop-types";
-import { EDITOR_API_KEY } from "../config";
+import { Editor } from '@tinymce/tinymce-react';
+import { PropTypes } from 'prop-types';
+import { EDITOR_API_KEY } from '../config';
 
-const TextEditor = ({ height, value, setValue, initialValue = "" }) => {
-    return (
-        <Editor
-            value={value}
-            apiKey={EDITOR_API_KEY}
-            init={{
-                plugins:
-                    "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
-                toolbar:
-                    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-                tinycomments_mode: "embedded",
-                tinycomments_author: "Author name",
-                height,
-            }}
-            onEditorChange={(newValue, editor) => setValue(newValue)}
-            initialValue={initialValue}
-        />
-    );
+const TextEditor = ({ height, value, setValue, initialValue = '' }) => {
+  return (
+    <Editor
+      value={value}
+      apiKey={EDITOR_API_KEY}
+      init={{
+        selector: 'textarea',
+        visual: false,
+        plugins:
+          'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+        toolbar:
+          'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        height,
+      }}
+      onEditorChange={(newValue, editor) => {
+        setValue(newValue);
+      }}
+      initialValue={initialValue}
+    />
+  );
 };
 
 TextEditor.propTypes = {
-    value: PropTypes.string.isRequired,
-    setValue: PropTypes.func.isRequired,
-    height: PropTypes.number.isRequired,
-    initialValue: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
+  height: PropTypes.number.isRequired,
+  initialValue: PropTypes.string,
 };
 
 export default TextEditor;
+
--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,3 @@
 export const EDITOR_API_KEY = import.meta.env.VITE_TEXT_EDITOR_API_KEY;
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-export const USER_ACTIONS = {
-  postQuestion: 'postQuestion',
-  postAnswer: 'postAnswer',
-  markCorrectAnswer: 'markCorrectAnswer',
-  markCorrectAnswerUser: 'markCorrectAnswerUser',
-  upvoteQuestion: 'upvoteQuestion',
-  upvoteAnswer: 'upvoteAnswer',
-  vote: 'vote',
-  addTag: 'addTag',
-  postEvent: 'postEvent',
-  downvoteQuestion: 'downvoteQuestion',
-  downvoteAnswer: 'downvoteAnswer',
-  deleteAnswer: 'deleteAnswer',
-  deleteQuestion: 'deleteQuestion',
-  removeVote: 'removeVote',
-};
-
